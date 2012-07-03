9a45cd7eb46ada89b3e3fd542b74b48a9ac9e5dc
Ron <cips173@gmail.com>
2012-07-03 13:34:06 +0530

    add again camear with lots of fixes like Should fix camera preview working camera and more

--- Android.mk
+++ Android.mk
@@ -17,5 +17,5 @@
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),pecan)
 include $(call first-makefiles-under,$(call my-dir))
 endif
-  include $(call all-named-subdir-makefiles, recovery gadget_id hwaddrs libcopybit liblights netd bdaddr_read)
+  include $(call all-named-subdir-makefiles, recovery gadget_id hwaddrs libcamera libcopybit liblights netd bdaddr_read)
 #endif
